land-msm-3-18/android_external_pulse

Pulse: Use white as default custom color
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/android/systemui/navigation/pulse/ColorController.java
@@ -92,8 +92,7 @@ public class ColorController extends ContentObserver
         mColorType = Settings.System.getIntForUser(resolver,
                 Settings.System.PULSE_COLOR_TYPE, COLOR_TYPE_ACCENT, UserHandle.USER_CURRENT);
         mColor = Settings.System.getIntForUser(resolver,
-                Settings.System.PULSE_COLOR_USER,
-                getAccentColor(),
+                Settings.System.PULSE_COLOR_USER, 0xFFFFFFFF,
                 UserHandle.USER_CURRENT);
         int lava_speed = Settings.System.getIntForUser(resolver,
                 Settings.System.PULSE_LAVALAMP_SPEED, LAVA_LAMP_SPEED_DEF,

ColtOSTemp/platform_external_pulse

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/systemui/navigation/pulse/ColorController.java
@@ -92,8 +92,7 @@ public class ColorController extends ContentObserver
         mColorType = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.PULSE_COLOR_MODE, COLOR_TYPE_ACCENT, UserHandle.USER_CURRENT);
         mColor = Settings.Secure.getIntForUser(resolver,
-                Settings.Secure.PULSE_COLOR_USER,
-                getAccentColor(),
+                Settings.Secure.PULSE_COLOR_USER, 0xFFFFFFFF,
                 UserHandle.USER_CURRENT);
         int lava_speed = Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.PULSE_LAVALAMP_SPEED, LAVA_LAMP_SPEED_DEF,